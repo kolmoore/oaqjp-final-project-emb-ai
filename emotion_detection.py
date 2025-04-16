5dad996c5c0c99b09989740eb2c7a6064b02a751
kolmoore <kolmoore@iastate.edu>
2025-04-16 11:54:46 -0400

    Task 3 Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
 
@@ -8,4 +9,23 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json = input_json, headers=header, timeout=10)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
+    emotions = {'anger': anger_score, 
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score
+                }
+    dominant = max(emotions, key=emotions.get)
+
+    # Add the dominant emotion to the dictionary
+    emotions['dominant_emotion'] = dominant
+
+    return emotions
